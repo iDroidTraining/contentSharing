c1a94e04857c5d8bd99f9d1d19b5e29ed61e4ae9
Mabualzait <m.abualzait@gmail.com>
2018-09-10 10:50:59 +0300

    checkstyle added

--- app/src/main/java/ws/idroid/contentsharing/MainActivity.java
+++ app/src/main/java/ws/idroid/contentsharing/MainActivity.java
@@ -16,18 +16,28 @@ import android.widget.*;
 
 import java.io.File;
 
+import butterknife.*;
+
 public class MainActivity extends AppCompatActivity {
 
     //Link for receiving content goo.gl/iw8hnG
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
+    private static final String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     private String imagePath;
-    private EditText etTextToShare;
-    private ImageView btn_select;
+
+    @BindView(R.id.et_share_text)
+    EditText etTextToShare;
+
+    private ImageView btnSelect;
+
+    @OnClick(R.id.btn_share_picture)
+    void onImagePressed() {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
 
     public static void verifyStoragePermissions(Activity activity) {
         // Check if we have write permission
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         verifyStoragePermissions(MainActivity.this);
         ActivityCompat.requestPermissions(
                 MainActivity.this,
@@ -60,12 +71,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findViews() {
-        etTextToShare = findViewById(R.id.et_share_text);
         Button btnShareText = findViewById(R.id.btn_share_text);
-        btn_select = findViewById(R.id.iv_share_select);
+        btnSelect = findViewById(R.id.iv_share_select);
         Button btnSharePicture = findViewById(R.id.btn_share_picture);
         btnShareText.setOnClickListener(view -> shareTextUri());
-        btn_select.setOnClickListener(view -> {
+        btnSelect.setOnClickListener(view -> {
             Intent intent = new Intent();
             // Show only images, no videos or anything else
             intent.setType("image/*");
@@ -79,7 +89,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void shareTextUri() {
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Ghazza Course Title");
         shareIntent.putExtra(Intent.EXTRA_TEXT, etTextToShare.getText().toString());
@@ -94,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             Log.i("image path Uri", "path = " + selectedImageUri);
             imagePath = getPath(selectedImageUri);
             Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-            btn_select.setImageBitmap(bitmap);
+            btnSelect.setImageBitmap(bitmap);
         }
     }
 
